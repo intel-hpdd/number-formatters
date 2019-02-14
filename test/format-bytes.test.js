c75c6110f0fb4fcb4f755d4a9e87b9eecb455b79
Will Johnson <wjohnson@whamcloud.com>
2019-02-14 17:05:20 -0500

    Units are displayed in decimal but should be displayed in binary
    Fixes #6.
    
    The storage metrics are displayed using incorrect units. A summary of how this is calculated is as follows:
    
    Values are calculated using binary conversion. the labels should transform to the following:
    
    kibibyte => kibibyte
    megabyte => mebibyte
    gigabyte => gibibyte
    terabyte => tebibyte
    petabyte => pebibyte
    exabyte => exbibyte
    zettabyte => zebibyte
    yottabyte => yobibyte
    
    Signed-off-by: Will Johnson <wjohnson@whamcloud.com>

--- test/format-bytes.test.js
+++ test/format-bytes.test.js
@@ -12,23 +12,23 @@ describe('Format bytes', function() {
       },
       {
         in: [200000],
-        out: '195.3 kB'
+        out: '195.3 KiB'
       },
       {
         in: [3124352],
-        out: '2.980 MB'
+        out: '2.980 MiB'
       },
       {
         in: [432303020202, 6],
-        out: '402.614 GB'
+        out: '402.614 GiB'
       },
       {
         in: [5323330102372, 3],
-        out: '4.84 TB'
+        out: '4.84 TiB'
       },
       {
         in: ['5323330102372', '3'],
-        out: '4.84 TB'
+        out: '4.84 TiB'
       },
       {
         in: [1000],
@@ -40,23 +40,23 @@ describe('Format bytes', function() {
       },
       {
         in: [1024, 0],
-        out: '1.000 kB'
+        out: '1.000 KiB'
       },
       {
         in: [4326, 5],
-        out: '4.2246 kB'
+        out: '4.2246 KiB'
       },
       {
         in: [3045827469],
-        out: '2.837 GB'
+        out: '2.837 GiB'
       },
       {
         in: [84567942345572238],
-        out: '75.11 PB'
+        out: '75.11 PiB'
       },
       {
         in: [5213456204567832146028],
-        out: '4.416 ZB'
+        out: '4.416 ZiB'
       },
       {
         in: [NaN],
