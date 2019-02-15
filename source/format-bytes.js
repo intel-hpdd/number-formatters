d562ef40349919ff566a16752d919c7157c13134
Will Johnson <wjohnson@whamcloud.com>
2019-02-15 17:07:42 -0500

    Units are displayed in decimal but should be displayed in binary (#7)
    * Units are displayed in decimal but should be displayed in binary
    
    Fixes #6.
    
    The storage metrics are displayed using incorrect units. A summary of how this is calculated is as follows:
    
    Values are calculated using binary conversion. the labels should transform to the following:
    
    kibibyte => kibibyte
    megabyte => mebibyte
    gigabyte => gibibyte
    terabyte => tebibyte
    petabyte => pebibyte
    exabyte => exbibyte
    zettabyte => zebibyte
    yottabyte => yobibyte
    
    Signed-off-by: Will Johnson <wjohnson@whamcloud.com>
    
    * Remove yarn lockfile
    
    Signed-off-by: Will Johnson <wjohnson@whamcloud.com>

--- source/format-bytes.js
+++ source/format-bytes.js
@@ -5,7 +5,7 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
-const units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+const units = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
 
 export default (bytes: number, precision: number) => {
   if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '';
